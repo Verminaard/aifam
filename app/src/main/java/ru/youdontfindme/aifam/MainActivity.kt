6c6111768231860737d978a6fc5e5eb84e0cbefd
Baryshnikov Oleg <ob@digipeople.ru>
2021-01-31 20:53:14 +0100

    feat: add basic navigation elements

--- app/src/main/java/ru/youdontfindme/aifam/MainActivity.kt
+++ app/src/main/java/ru/youdontfindme/aifam/MainActivity.kt
@@ -2,6 +2,11 @@ package ru.youdontfindme.aifam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.main_activity.*
 import ru.youdontfindme.aifam.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,9 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationMenu?.setupWithNavController(navController)
     }
 }
